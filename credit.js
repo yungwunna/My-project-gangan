f0f3f74c83b1946cf42308d30cb0fb94c2f1be0f
Benson <israelbenson5802@gmail.com>
2023-03-02 16:22:28 -0800

    first commit

--- credit.js
+++
@@ -1,9 +0,0 @@
-let stuff = document.getElementsByClassName("content");
-console.log(stuff);
-let image = document.getElementById("img")
-const changes = [
-    {
-   text: 'a town hall, different from bala blu, blu  blu.',
-   image:  
-    }
-];
